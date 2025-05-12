07258c47604e3777208d754a946c83057bf68ed1
David-del-pix <mapangaladavid@gmail.com>
2025-05-12 21:24:51 +0530

    Time: 3 ms (47.5%), Space: 46.1 MB (13.6%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int farthest = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i > farthest) return false; // Can't reach this position
+            farthest = Math.max(farthest, i + nums[i]);
+        }
+
+        return true;
+    }
+}
